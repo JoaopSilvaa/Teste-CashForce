81240afe06febe1875a7a152764cdb9c50d2f52a
Joao Antonio <joaopsilva.dev@gmail.com>
2022-12-26 14:24:00 -0300

    feat: :construction: criando services da model provider

--- app/backend/src/services/providersServices.js
+++ app/backend/src/services/providersServices.js
@@ -14,6 +14,98 @@ const readProviderById = async (id) => {
     return provider;
 };
 
+const readAllProviders = async () => {
+    const providers = await Provider.findAll();
+    
+    if (!providers) {
+        return {
+            error: {
+                code: 'notFound',
+                message: 'Providers not Found',
+            },
+        };
+    }
+    return providers;
+};
+
+const verifyAlreadyExist = async (name) => {
+    const providerInDB = await Provider.findAll({
+        where: { name },
+    });
+
+    if (providerInDB.length !== 0) {
+        return {
+            error: {
+                code: 'conflict',
+                message: 'Provider already registered',
+            },
+        };
+    }
+};
+
+const createProvider = async (provider) => {   
+    const params = ['name', 'tradingName'];
+    const paramsOrder = Object.keys(provider); 
+
+    const verifyParams = params.every((element) => paramsOrder.includes(element));
+    if (!verifyParams) {
+        return {
+            error: { code: 'badRequest', message: 'Parameters are missing' },
+        };
+    }
+
+    const { name } = provider;
+
+    const providerExist = await verifyAlreadyExist(name);
+    if (providerExist) return providerExist;
+
+    return Provider.create(provider);
+};
+
+const updateProvider = async (provider, id) => {
+    const providerExist = await Provider.findByPk(id);
+    
+    if (!providerExist) {
+        return {
+            error: {
+                code: 'notFound',
+                message: 'Provider not Found',
+            },
+        };
+    }
+
+    const { name, tradingname, cashforceTax, responsibleName } = provider;
+
+    await Provider.update({
+            name,
+            tradingname,
+            cashforceTax,
+            responsibleName,
+        },
+        { where: { id } });
+
+    return Provider.findByPk(id);
+};
+
+const deleteProvider = async (id) => {
+    const providerExist = await Provider.findByPk(id);
+    
+    if (!providerExist) {
+        return {
+            error: {
+                code: 'notFound',
+                message: 'Provider not Found',
+            },
+        };
+    }
+
+    return Provider.destroy({ where: { id } });
+};
+
 module.exports = {
+    readAllProviders,
     readProviderById,
+    createProvider,
+    updateProvider,
+    deleteProvider,
 };
