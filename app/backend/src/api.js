dcb8d07e76c48acd4ccdbd1cda56beee6563f999
Joao Antonio <joaopsilva.dev@gmail.com>
2022-12-26 16:19:15 -0300

    feat: :construction: instalando cors

--- app/backend/src/api.js
+++ app/backend/src/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const rescue = require('express-rescue');
 const ordersRouter = require('./routes/ordersRouter');
 const providerRouter = require('./routes/providersRouter');
@@ -7,6 +8,8 @@ const errors = require('./middlewares/errors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use('/orders', rescue(ordersRouter));
 app.use('/providers', rescue(providerRouter));
 
