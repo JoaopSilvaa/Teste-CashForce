c01817891015ff618c53d4799c08b7e157d6d01e
Joao Antonio <joaopsilva.dev@gmail.com>
2022-12-27 17:33:15 -0300

    feat: :construction: finalizando docker-compose e dockerfile do frontend

--- app/backend/src/database/config/config.js
+++ app/backend/src/database/config/config.js
@@ -8,12 +8,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'teste-cashforce'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '1234',
+    `${process.env.DB_NAME || 'teste-cashforce'}${suffix[environment] || suffix.test}`,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
